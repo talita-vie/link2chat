4f360ed72f90bad926d556efa2cc57dce23bfb9b
Talita <talitavieira.dsg@gmail.com>
2025-12-28 15:22:24 -0300

    chore: initial project setup with working Chrome extension
    - cleaned default files (removed src/App.css)
    
    - configured App.jsx
    
    - created public/manifest.json (Manifest V3 compliant)
    
    - generated build and verified functionality in Chrome

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ width: '300px', padding: '20px' }}>
+      <h1>Link2Chat</h1>
+      <p>Minha primeira extensão!</p>
+    </div>
+  );
+}
+
+export default App;
